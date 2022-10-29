95e46ec26e0e475d816140ec53af2c81e3b26e57
Marc Michalsky <michalsky@forumzfd.de>
2022-10-29 23:02:36 +0200

    🔖 bump version 1.0.3 -> 1.0.4

--- src/civiproxy_logs2json/__init__.py
+++ src/civiproxy_logs2json/__init__.py
@@ -5,7 +5,7 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 _cfg = tomllib.loads(resources.read_text("civiproxy_logs2json", "config.toml"))
 REQUEST_LINE_RE = _cfg["regex"]["request_line"]
